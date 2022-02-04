3faba85044f5f06d7545bdf6a5d1a4aad95b9938
Teacher A <teaching.know@gmail.com>
2022-02-04 08:58:54 +0100

    Update application.rb

--- lib/app/application.rb
+++ lib/app/application.rb
@@ -5,7 +5,7 @@ class Application
   def perform
     "\n"
   	# Démarrer la première partie
-    puts "BIENVENUE!! Vous allez jouer au 🕹️  MORPION🕹️ ".
+    puts "BIENVENUE!! Vous allez jouer au 🕹️  MORPION🕹️ ".center(100).magenta
     @game = Game.new
     @game.on_going
   end
